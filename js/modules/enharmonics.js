c2729d71f7952760e28afad3917cc86f230c4ab9
Jim <jimkernicky@gmail.com>
2023-10-12 09:36:07 -0400

    docs: Add JSDoc comments to enharmonics

--- js/modules/enharmonics.js
+++ js/modules/enharmonics.js
@@ -1,47 +1,54 @@
 import { checkIndices, chordsFound } from "./checkIndices.js";
 import { sharp, flat } from "../data/constants.js";
 
-export default function enharmonics(object, arr, arr2) {
-  checkIndices(arr2)
+/**
+ * 8. Handle edge cases/exceptions: enharmonic equivalents
+ * 
+ * 
+ * @param {object} noteTones 
+ * @param {array} octave 
+ * @param {array} intervals 
+ */
+export default function enharmonics(noteTones, octave, intervals) {
+  checkIndices(intervals)
   if (chordsFound.length > 0) {
-    // 9. if blocks to handle special cases such as enharmonic equivalents
       // Fix flat 9's if the ♭9 is a sharp
-      if (object.hasOwnProperty(1) && object[1].length === 2) {
-        arr.splice(1, 1, arr[2] + flat);
-        object[1] = arr[1];
+      if (noteTones.hasOwnProperty(1) && noteTones[1].length === 2) {
+        octave.splice(1, 1, octave[2] + flat);
+        noteTones[1] = octave[1];
       }
 
       // Fix sharp 9's / flat 3's
-      if (object.hasOwnProperty(3) && !object.hasOwnProperty(4) && object[3].length === 2) {
-        arr.splice(3, 1, arr[4] + flat);
-        object[3] = arr[3];
+      if (noteTones.hasOwnProperty(3) && !noteTones.hasOwnProperty(4) && noteTones[3].length === 2) {
+        octave.splice(3, 1, octave[4] + flat);
+        noteTones[3] = octave[3];
       }
-      if (object.hasOwnProperty(3) && object.hasOwnProperty(4) && object[3].length === 2 && flatRadioBtn.checked) {
-        arr.splice(3, 1, arr[2] + sharp);
-        object[3] = arr[3];
+      if (noteTones.hasOwnProperty(3) && noteTones.hasOwnProperty(4) && noteTones[3].length === 2 && flatRadioBtn.checked) {
+        octave.splice(3, 1, octave[2] + sharp);
+        noteTones[3] = octave[3];
       }
 
       // Fix sharp 11's / flat 5's
-      if (object.hasOwnProperty(6) && !object.hasOwnProperty(7) && object[6].length === 2) {
-        arr.splice(6, 1, arr[7] + flat);
-        object[6] = arr[6];
+      if (noteTones.hasOwnProperty(6) && !noteTones.hasOwnProperty(7) && noteTones[6].length === 2) {
+        octave.splice(6, 1, octave[7] + flat);
+        noteTones[6] = octave[6];
       }
 
       // Fix sharp 5's / flat 13's
-      if (object.hasOwnProperty(7) && object.hasOwnProperty(8) && object[8].length === 2) {
-        arr.splice(8, 1, arr[9] + flat);
-        object[8] = arr[8];
+      if (noteTones.hasOwnProperty(7) && noteTones.hasOwnProperty(8) && noteTones[8].length === 2) {
+        octave.splice(8, 1, octave[9] + flat);
+        noteTones[8] = octave[8];
 
       }
-      if (!object.hasOwnProperty(7) && object.hasOwnProperty(8) && object[8].length === 2 && flatRadioBtn.checked) {
-        arr.splice(8, 1, arr[7] + sharp);
-        object[8] = arr[8];
+      if (!noteTones.hasOwnProperty(7) && noteTones.hasOwnProperty(8) && noteTones[8].length === 2 && flatRadioBtn.checked) {
+        octave.splice(8, 1, octave[7] + sharp);
+        noteTones[8] = octave[8];
       }
 
       // Fix flat 7's for the keys F & C if sharpRadioBtn.checked
-      if (object.hasOwnProperty(10) && object[10].length === 2 && arr[0].length === 1) {
-        arr.splice(10, 1, arr[11] + flat);
-        object[10] = arr[10];
+      if (noteTones.hasOwnProperty(10) && noteTones[10].length === 2 && arr[0].length === 1) {
+        octave.splice(10, 1, octave[11] + flat);
+        noteTones[10] = octave[10];
       }
   }
 }
