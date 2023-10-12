11945b6108d90f25cd22bd9498efdad46bdd3054
Jim <jimkernicky@gmail.com>
2023-10-12 11:17:51 -0400

    docs: Add JSDoc comment to outputToDom

--- js/modules/outputToDom.js
+++ js/modules/outputToDom.js
@@ -1,10 +1,16 @@
 const elementNames = ['slashChordName', 'userNotes', 'chordNotes', 'chordName', 'chordIntervals', 'chordTendency', 'equalChords'];
 const elementIds = ['slash-chord-name', 'user-notes', 'chord-notes', 'chord-name', 'chord-intervals', 'chord-tendency', 'equal-chords'];
 
-export default function outputToDom(arr) {
+/**
+ * This is a more compact way other than using getElementById, 
+ * createTextNode, and append individually in script.js
+ * 
+ * @param {array} vars The variables for 7 of the vars ouput to the DOM
+ */
+export default function outputToDom(vars) {
   elementNames.forEach((name, i) => {
     name = document.getElementById(elementIds[i]);
-    const text = document.createTextNode(arr[i]);
+    const text = document.createTextNode(vars[i]);
     name.append(text);
   });
 }
