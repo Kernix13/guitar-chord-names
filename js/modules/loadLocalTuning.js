3608fbc53b9741c5d3287f7f51cdfdbe7fadfb7c
Jim <jimkernicky@gmail.com>
2023-10-12 14:46:26 -0400

    docs: Add JSDoc comment in loadLocalTuning

--- js/modules/loadLocalTuning.js
+++ js/modules/loadLocalTuning.js
@@ -1,5 +1,4 @@
-const notesFormElement = document.getElementById("notes-form");
-const numberInputElements = notesFormElement.querySelectorAll('.note');
+const numberInputElements = document.querySelectorAll('.note');
 
 // Get the elements that show the notes for each tuning
 const sixth = document.getElementById("sixth");
@@ -9,16 +8,18 @@ const third = document.getElementById("third");
 const second = document.getElementById("second");
 const first = document.getElementById("first");
 
-// Check local storage and load tuning values
+/**
+ * Check local storage and load tuning values. Use innerText to 
+ * initially load the tuning if not in localStorage, else use
+ * the value in localStorage to set the innerText
+ */
 export default function loadLocalTuning() {
-  // use innerText to initially load the tuning
   if (localStorage.getItem('userStrings') === null) {
     const selectedTuningInLocalStorage = [sixth.innerText, fifth.innerText, fourth.innerText, third.innerText, second.innerText, first.innerText];
     localStorage.setItem("userStrings", selectedTuningInLocalStorage);
 
     numberInputElements[0].focus();
   } else {
-    // but in tuning exists in localStorage, then set the innerText
     const selectUserStrings = localStorage.getItem("userStrings");
 
     sixth.innerText = selectUserStrings.split(",")[0];
