2a220e55dba869329aa6838db170df3fd7ab3ff1
Jim <jimkernicky@gmail.com>
2023-10-12 11:44:06 -0400

    docs: Added JSDoc comment in noMatchError

--- js/modules/noMatchError.js
+++ js/modules/noMatchError.js
@@ -1,8 +1,16 @@
-export default function noMatchError(arr1, arr2, arr3) {
-  if (arr1.length >= 3 && arr2.length === 0) {
+/**
+ * Return an error if the 3 or more unique notes does not match
+ * a chord in chord-intervals.js
+ * 
+ * @param {array} intervals The intervals for the unique notes
+ * @param {array} matches Chords found in chord-intervals.js
+ * @param {array} notes The unique notes
+ */
+export default function noMatchError(intervals, matches, notes) {
+  if (intervals.length >= 3 && matches.length === 0) {
     const errorOutput = document.getElementById("error-output");
     const errorMsg = "That is not a valid chord or it is not in our database. Check the tuning or sharp/flat key buttons.";
     errorOutput.innerHTML = `<p>` + errorMsg + `</p>`;
-    document.getElementById("user-notes").innerHTML = arr3.join("-");
+    document.getElementById("user-notes").innerHTML = notes.join("-");
   }
 }
