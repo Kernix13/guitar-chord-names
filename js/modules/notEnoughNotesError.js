6f75820cd6ea19bd55e7245cde350e773c499046
Jim <jimkernicky@gmail.com>
2023-10-12 11:25:39 -0400

    docs: Added JSDoc comment to notEnoughNotesError

--- js/modules/notEnoughNotesError.js
+++ js/modules/notEnoughNotesError.js
@@ -1,6 +1,11 @@
-export default function notEnoughNotesError(arr2) {
-  if (arr2.length < 3) {
-    const userNotes = arr2.join("-");
+/**
+ * Error message if the user entered only 1 or 2 unique notes
+ * 
+ * @param {array} notes 
+ */
+export default function notEnoughNotesError(notes) {
+  if (notes.length < 3) {
+    const userNotes = notes.join("-");
     const errorOutput = document.getElementById("error-output");
     const errorMsg = "That is not a chord. Enter at least 3 unique chord tones.";
     errorOutput.innerHTML = `<p>${errorMsg}</p>`;
