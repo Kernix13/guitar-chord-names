dfd7455b90ae1433f8de6629db37406e0a9b292f
Jim <jimkernicky@gmail.com>
2023-10-13 18:09:16 -0400

    Edited error message

--- js/modules/notEnoughNotesError.js
+++ js/modules/notEnoughNotesError.js
@@ -5,7 +5,7 @@
  */
 export default function notEnoughNotesError(notes) {
     const errorOutput = document.getElementById("error-output");
-    const errorMsg = "That is not a chord. Enter at least 3 unique chord tones.";
+    const errorMsg = "That is not a chord. Enter at least 3 unique chord tones or check your sharp/flat selection.";
     errorOutput.innerHTML = `<p>${errorMsg}</p>`;
     document.getElementById("user-notes").innerHTML = notes;
 }
